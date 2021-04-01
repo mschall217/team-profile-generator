88b0e4fdcf62093c063c9b4b281974ee7edbcdac
Morgan Schall <morgan.allison.schall@gmail.com>
2021-04-01 09:37:01 -0400

    got the questions working in a cycle with functions to generate team members

--- index.js
+++ index.js
@@ -23,14 +23,117 @@ const promptUser = () => {
     ])
     .then(function(data){
         if(data.makeTeam === 'yes'){
-            addTeamMember();
+            addEmployee();
         }
         if(data.makeTeam === 'no'){
             return;
         }
     })
   }
-  
+  const addEmployee = () => {
+    inquirer.prompt([
+        {
+           type: 'list',
+           name: 'addEmployee',
+           message: 'Would you like to add an employee?',
+           choices: [
+               "yes",
+               "no"
+           ]
+        }
+       ])
+       .then(function(data){
+           if(data.addEmployee === 'yes'){
+             addTeamMember();
+           }
+           if(data.addEmployee === 'no'){
+               return;
+           }
+       })
+  }
+  const addTeamMember = () => {
+    inquirer.prompt([
+        {
+           type: 'input',
+           name: 'name',
+           message: 'What is the team members name',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team members ID number',
+         } ,       
+         {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team members email address?',
+         },
+         {
+            type: 'list',
+            name: 'role',
+            message: 'What is the team members role',
+            choices: [
+                'Manager',
+                'Engineer',
+                'Intern',
+            ]
+         }
+       ])
+       .then(function(data){
+        if(data.role === 'Manager'){
+            addManager(data.name, data.id, data.email);
+        }
+        if(data.role == 'Engineer'){
+            addEngineer(data.name, data.id, data.email);
+        }
+        if(data.role === 'Intern'){
+            addIntern(data.name, data.id, data.email);
+        }
+    })
+
+    }
+
+    const addManager = (name, id, email) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'officeNum',
+                message: 'Please enter your managers number'
+            }
+        ])
+        .then(function(data){
+            console.log(`Managers name is ${name} id is ${id} email ${email} Phone number is ${data.officeNum}`);
+            addEmployee();
+        })
+    }
+
+    const addEngineer = (name, id, email) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Please enter your github username'
+            }
+        ])
+        .then(function(data){
+            console.log(`Engineers name is ${name} id is ${id} email ${email} GitHub is github.com/${data.github}`);
+            addEmployee();
+        })
+    }
+
+    const addIntern = (name, id, email) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Please enter what school you are attending'
+            }
+        ])
+        .then(function(data){
+            console.log(`Interns name is ${name} id is ${id} email ${email} is attending ${data.school}`);
+            addEmployee();
+        })
+    }
 
 
   const init = () => {
