974e8599d65da4bc0d049bb0718fae6ce56ecc1a
Morgan Schall <morgan.allison.schall@gmail.com>
2021-04-01 09:08:41 -0400

    made the program ask if they want to build a team if yes then it does if no it stops

--- index.js
+++ index.js
@@ -2,24 +2,44 @@ const inquirer = require('inquirer');
 //what we use to get the data
 const fs = require('fs');
 //allows us to write files
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+
+let team = [];
 
 const promptUser = () => {
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Enter your name!',
-      },
+    inquirer.prompt([
+     {
+        type: 'list',
+        name: 'makeTeam',
+        message: 'Would you like to make a team profile?',
+        choices: [
+            "yes",
+            "no"
+        ]
+     }
     ])
+    .then(function(data){
+        if(data.makeTeam === 'yes'){
+            addTeamMember();
+        }
+        if(data.makeTeam === 'no'){
+            return;
+        }
+    })
   }
   
+
+
   const init = () => {
     promptUser()
-    .then((answers) => {
-      fs.writeFile('index.html', generateMarkdown(answers), (err) =>
-        err ? console.log(err) : console.log('Successfully created a team profile page!')
-      );
-    });
+    // .then((answers) => {
+    //   fs.writeFile('index.html', generateMarkdown(answers), (err) =>
+    //     err ? console.log(err) : console.log('Successfully created a team profile page!')
+    //   );
+    // });
   }
   
   init()
